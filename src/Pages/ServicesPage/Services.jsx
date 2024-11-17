ae890c7a38834187960f39893753f45b094f2e41
ramchandragawade <raajgawade39@gmail.com>
2024-11-17 14:27:08 +0530

    services section done

--- src/Pages/ServicesPage/Services.jsx
+++ src/Pages/ServicesPage/Services.jsx
@@ -1,10 +1,25 @@
 import './Services.css';
 import TitleBox from '../../components/TitleBox/TitleBox';
+import servicesData from '../../assets/services_data';
+import arrowIcon from '../../assets/arrow_icon.svg';
 
 const Services = () => {
   return (
     <div className='services'>
         <TitleBox title={'Services'}/>
+        <div className="services-container">
+          {servicesData.map((service, index)=>{
+            return <div className="services-format" key={index}>
+              <h3>{service.s_no}</h3>
+              <h2>{service.s_name}</h2>
+              <p>{service.s_desc}</p>
+              <div className="services-readmore">
+                <p>Read More</p>
+                <img src={arrowIcon} alt="" />
+              </div>
+            </div>
+          })}
+        </div>
     </div>
   )
 }
