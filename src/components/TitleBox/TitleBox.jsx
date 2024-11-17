b7d1aad737ad030b14de157ddd72c17ede5e36f5
ramchandragawade <raajgawade39@gmail.com>
2024-11-17 13:58:41 +0530

    Title box changes

--- src/components/TitleBox/TitleBox.jsx
+++ src/components/TitleBox/TitleBox.jsx
@@ -2,10 +2,12 @@ import './TitleBox.css';
 import themePattern from '../../assets/theme_pattern.svg';
 const TitleBox = ({title}) => {
   return (
+	<div className="title-container">
 		<div className="title-box">
 			<h1>{title}</h1>
 			<img src={themePattern} alt="theme pattern" />
 		</div>
+	</div>
   );
 }
 
