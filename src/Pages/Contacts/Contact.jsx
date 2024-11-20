5f3ebbd2bbaee7ea94c9aa600fa40a9163430d91
ramchandragawade <raajgawade39@gmail.com>
2024-11-21 01:26:04 +0530

    send message via contact form

--- src/Pages/Contacts/Contact.jsx
+++ src/Pages/Contacts/Contact.jsx
@@ -3,9 +3,42 @@ import './Contact.css'
 import mailIcon from '../../assets/mail_icon.svg';
 import callIcon from '../../assets/call_icon.svg';
 import locIcon from '../../assets/location_icon.svg';
+import emailjs from '@emailjs/browser';
+import { useRef, useState } from 'react';
 
 const Contact = () => {
-  return (
+    const formRef = useRef();
+    const [error, setError] = useState({
+        error: null,
+        message: ''
+    });
+    const sendEmail = (e) => {
+        e.preventDefault();
+        emailjs.sendForm('service_t9rle5k', 'template_d7ejzfe', formRef.current, {
+            publicKey: 'on5wRM5GZEaoF0UUE',
+        }).then(
+            () => {
+                setError({
+                    error: false,
+                    message: 'Successfully Sent!'
+                });
+                setTimeout(()=>{
+                    setError({
+                        error: null,
+                        message: ''
+                    })
+                },3000);
+                formRef.current.reset();
+            },
+            (error) => {
+                setError({
+                    error: true,
+                    message: 'Error while sending email!'
+                });
+            },
+        );
+    }
+    return (
     <div id='contact' className='contact'>
         <TitleBox title={'Get in touch'}/>
         <div className="contact-section">
@@ -27,17 +60,17 @@ const Contact = () => {
                     </div>
                 </div>
             </div>
-            <form className='contact-right'>
+            <form className='contact-right' onSubmit={sendEmail} ref={formRef}>
                 <label htmlFor="name">Your Name</label>
-                <input type="text" placeholder='Enter your name' name='name' id='name'/>
+                <input type="text" placeholder='Enter your name' name='name' id='name' required/>
 
                 <label htmlFor="">Your Email</label>
-                <input type="email" placeholder='Enter your email id' name='email' id='email'/>
+                <input type="email" placeholder='Enter your email id' name='email' id='email' required/>
 
                 <label htmlFor="message">Write your message here</label>
-                <textarea name="message" id="message" placeholder='Enter your message' rows={8}></textarea>
+                <textarea name="message" id="message" placeholder='Enter your message' rows={8} required></textarea>
                 <button className='contact-submit' type='submit'>Submit now</button>
-
+                {error.error !== null && <p className='error-cls' style={{ color: error.error ?'red':'green'}} >{error.message}</p> }
             </form>
         </div>
     </div>
