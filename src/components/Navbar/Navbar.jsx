2233ed3cfc6d76ffbc43fbbd78e3aa4993cff4a1
ramchandragawade <raajgawade39@gmail.com>
2024-11-21 00:47:47 +0530

    added active img to nav li

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,19 @@
 import './Navbar.css';
 import logo from "../../assets/logo.svg"
 import PatternBtn from '../Buttons/PatternBtn';
+import { useState } from 'react';
+import underlineImg from '../../assets/nav_underline.svg';
 const Navbar = () => {
+  const [menu, setMenu] = useState('home'); 
   return (
     <div className='navbar'>
         <img src={logo} alt="" />
         <ul className='nav-menu'>
-            <li>Home</li>
-            <li>About me</li>
-            <li>Services</li>
-            <li>Projects</li>
-            <li>Contact</li>
+            <li><p onClick={()=>{setMenu('home')}}>Home</p>{menu==='home' && <img src={underlineImg} alt="Home" /> }</li>
+            <li><p onClick={()=>{setMenu('about')}}>About me</p>{menu==='about' && <img src={underlineImg} alt="Home" /> }</li>
+            <li><p onClick={()=>{setMenu('services')}}>Services</p>{menu==='services' && <img src={underlineImg} alt="Home" /> }</li>
+            <li><p onClick={()=>{setMenu('projects')}}>Projects</p>{menu==='projects' && <img src={underlineImg} alt="Home" /> }</li>
+            <li><p onClick={()=>{setMenu('contact')}}>Contact</p>{menu==='contact' && <img src={underlineImg} alt="Home" /> }</li>
         </ul>
       <PatternBtn>Connect With Me</PatternBtn>
     </div>
