873670d660194d0f5e88ed460885b73ffe6edfc2
ramchandragawade <raajgawade39@gmail.com>
2024-11-21 23:41:25 +0530

    added new hero svg

--- src/Pages/Hero/Hero.jsx
+++ src/Pages/Hero/Hero.jsx
@@ -1,12 +1,13 @@
 import './Hero.css';
-import profileImg from '../../assets/profile_img.svg'
+import profileImg from '../../assets/profile_img.svg';
+import heroImg from '../../assets/mf-avatar.svg';
 import PatternBtn from '../../components/Buttons/PatternBtn';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 const Hero = () => {
   return (
     <div id='home' className='hero'>
-        <img src={profileImg} alt="Profile Image" />
+        <img src={heroImg} alt="Profile Image" />
         <h1><span>I am Alex Bennet</span>, frontend developer based in USA.</h1>
         <p>I am a frontend developer from California, USA with 10 years of experience in multiple companies like Microsoft, Tesla and Apple. </p>
         <div className='hero-action'>
