e2306ddb6ca9fa5ce7cecb8dc9b90a782669dbdb
ramchandragawade <raajgawade39@gmail.com>
2024-11-24 02:57:50 +0530

    setting up for exp edu page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar/Navbar"
 import About from "./Pages/About/About"
 import Contact from "./Pages/Contacts/Contact"
+import ExpEdu from "./Pages/ExpEdu/ExpEdu"
 import Footer from "./Pages/Footer/Footer"
 import Hero from "./Pages/Hero/Hero"
 import Services from "./Pages/ServicesPage/Services"
@@ -12,6 +13,7 @@ const App = () => {
       <div id="top"></div>
       <Navbar/>
       <Hero/>
+      {/* <ExpEdu/> */}
       <About/>
       <Services/>
       <Works/>
---
+++ src/Pages/ExpEdu/ExpEdu.jsx
@@ -0,0 +1,12 @@
+import TitleBox from '../../components/TitleBox/TitleBox';
+import './ExpEdu.css';
+
+const ExpEdu = () => {
+  return (
+    <section className='exp-edu'>
+        <TitleBox title={'Experience & Education'}/>
+    </section>
+  )
+}
+
+export default ExpEdu
