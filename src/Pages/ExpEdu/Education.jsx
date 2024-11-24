23cbddd59806c91b918a98dd94f133fadbf265cd
ramchandragawade <raajgawade39@gmail.com>
2024-11-25 04:42:05 +0530

    sep comp

---
+++ src/Pages/ExpEdu/Education.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Education = () => {
+	return (
+		<div className="p-2">
+			<h1 className="text-3xl my-5 font-semibold">Education</h1>
+		</div>
+	);
+};
+
+export default Education;
