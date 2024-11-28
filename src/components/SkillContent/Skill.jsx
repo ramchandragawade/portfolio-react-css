d1d1cc1738033f8a595515e28ec5ee561f96e7b6
ramchandragawade <raajgawade39@gmail.com>
2024-11-29 03:18:04 +0530

    skill fix & favicon added

--- src/components/SkillContent/Skill.jsx
+++ src/components/SkillContent/Skill.jsx
@@ -2,7 +2,7 @@ const Skill = ({name, logo}) => {
 	return (
 		<div className="flex flex-col items-center justify-center text-center text-sm lg:text-base bg-inherit shadow-lg shadow-[#B415FF] rounded-md p-2 m-1 min-w-24 max-w-24 lg:min-w-32 lg:max-w-32">
 			<img className="h-7 lg:h-10" src={logo} alt= {name} />
-			<p className="text-slate-200">{name!=='Studio'?name: <p><span className="hidden lg:inline">Android{' '}</span>Studio</p>}</p>
+			<p className="text-slate-200">{name!=='Studio'?name: <span><span className="hidden lg:inline">Android{' '}</span>Studio</span>}</p>
 		</div>
 	);
 };
