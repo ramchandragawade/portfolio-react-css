0ff85efc6d5a802644e724133e2bd3574628cef7
ramchandragawade <raajgawade39@gmail.com>
2024-12-09 06:19:35 +0530

    thinner line

--- src/components/Awards/Awards.jsx
+++ src/components/Awards/Awards.jsx
@@ -5,12 +5,12 @@ const Awards = () => {
 				<h1 className="text-2xl lg:text-4xl font-bold bg-custom-gradient bg-clip-text text-transparent">5+</h1>
 				<p className="text-sm lg:text-lg font-medium text-center">YEARS OF EXPERIENCE</p>
 			</div>
-			<p className="text-4xl">|</p>
+			<p className="text-4xl font-thin">|</p>
 			<div className="flex flex-col items-center justify-center gap-2 duration-500 hover:scale-110 transition-all">
 				<h1 className="text-2xl lg:text-4xl font-bold bg-custom-gradient bg-clip-text text-transparent">5+</h1>
 				<p className="text-sm lg:text-lg font-medium text-center">YEARS OF EXPERIENCE</p>
 			</div>
-			<p className="text-4xl">|</p>
+			<p className="text-4xl font-thin">|</p>
 			<div className="flex flex-col items-center justify-center gap-2 duration-500 hover:scale-110 transition-all">
 				<h1 className="text-2xl lg:text-4xl font-bold bg-custom-gradient bg-clip-text text-transparent">5+</h1>
 				<p className="text-sm lg:text-lg font-medium text-center">YEARS OF EXPERIENCE</p>
