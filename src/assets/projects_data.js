c2f43e56b90699cefe41aae073fe6a73f8b13c57
ramchandragawade <raajgawade39@gmail.com>
2025-06-16 05:27:59 +0530

    Added scribble nest to project list

--- src/assets/projects_data.js
+++ src/assets/projects_data.js
@@ -1,4 +1,5 @@
 import yelpCampImg from '../assets/yelp1.png';
+import scribbleNestImg from '../assets/scribble-nest.png'
 const Project_Data = [
     {
         id: 'p1',
@@ -12,12 +13,12 @@ const Project_Data = [
     },
     {
         id: 'p2',
-        title: 'YelpCamp!',
-        shortDesc: 'YelpCamp is a campground review and creation platform built with MongoDB, Express.js, Node.js, and integrated with MapBox, Passport.js, and Cloudinary for enhanced functionality.',
-        description: 'YelpCamp is a campground review and creation platform built with MongoDB, Express.js, Node.js, and integrated with MapBox, Passport.js, and Cloudinary for enhanced functionality.',
-        imgs: [yelpCampImg],
-        gitUrl: 'https://github.com/ramchandragawade/yelpcamp',
-        demoUrl: 'https://yelpcamp.ramchandragawade.in/',
+        title: 'Scribble Nest',
+        shortDesc: 'Scribble Nest – CRUD Notes App Built with the MERN Stack.',
+        description: 'Scribble Nest is a sleek and feature-rich Full Stack application built using the MERN stack – MongoDB, Express.js, React.js, and Node.js. It enables users to securely create, manage, and organize personal scribbles with ease. The app includes secure authentication, an intuitive user interface, real-time editing capabilities, the ability to pin important scribbles, search functionality, and more. Whether you are exploring full-stack development or looking to showcase a robust CRUD app with end-to-end functionality, Scribble Nest is an ideal project for your portfolio.',
+        imgs: [scribbleNestImg],
+        gitUrl: 'https://github.com/ramchandragawade/scribble-nest/',
+        demoUrl: 'https://scribblenest.ramchandragawade.in/',
         techStack: []
     },
     {
@@ -32,12 +33,12 @@ const Project_Data = [
     },
     {
         id: 'p4',
-        title: 'YelpCamp!',
-        shortDesc: 'YelpCamp is a campground review and creation platform built with MongoDB, Express.js, Node.js, and integrated with MapBox, Passport.js, and Cloudinary for enhanced functionality.',
-        description: 'YelpCamp is a campground review and creation platform built with MongoDB, Express.js, Node.js, and integrated with MapBox, Passport.js, and Cloudinary for enhanced functionality.',
-        imgs: [yelpCampImg],
-        gitUrl: 'https://github.com/ramchandragawade/yelpcamp',
-        demoUrl: 'https://yelpcamp.ramchandragawade.in/',
+        title: 'Scribble Nest',
+        shortDesc: 'Scribble Nest – CRUD Notes App Built with the MERN Stack.',
+        description: 'Scribble Nest is a sleek and feature-rich Full Stack application built using the MERN stack – MongoDB, Express.js, React.js, and Node.js. It enables users to securely create, manage, and organize personal scribbles with ease. The app includes secure authentication, an intuitive user interface, real-time editing capabilities, the ability to pin important scribbles, search functionality, and more. Whether you are exploring full-stack development or looking to showcase a robust CRUD app with end-to-end functionality, Scribble Nest is an ideal project for your portfolio.',
+        imgs: [scribbleNestImg],
+        gitUrl: 'https://github.com/ramchandragawade/scribble-nest/',
+        demoUrl: 'https://scribblenest.ramchandragawade.in/',
         techStack: []
     },
     {
@@ -51,13 +52,12 @@ const Project_Data = [
         techStack: []
     },
     {
-        id: 'p6',
-        title: 'YelpCamp!',
-        shortDesc: 'YelpCamp is a campground review and creation platform built with MongoDB, Express.js, Node.js, and integrated with MapBox, Passport.js, and Cloudinary for enhanced functionality.',
-        description: 'YelpCamp is a campground review and creation platform built with MongoDB, Express.js, Node.js, and integrated with MapBox, Passport.js, and Cloudinary for enhanced functionality.',
-        imgs: [yelpCampImg],
-        gitUrl: 'https://github.com/ramchandragawade/yelpcamp',
-        demoUrl: 'https://yelpcamp.ramchandragawade.in/',
+        title: 'Scribble Nest',
+        shortDesc: 'Scribble Nest – CRUD Notes App Built with the MERN Stack.',
+        description: 'Scribble Nest is a sleek and feature-rich Full Stack application built using the MERN stack – MongoDB, Express.js, React.js, and Node.js. It enables users to securely create, manage, and organize personal scribbles with ease. The app includes secure authentication, an intuitive user interface, real-time editing capabilities, the ability to pin important scribbles, search functionality, and more. Whether you are exploring full-stack development or looking to showcase a robust CRUD app with end-to-end functionality, Scribble Nest is an ideal project for your portfolio.',
+        imgs: [scribbleNestImg],
+        gitUrl: 'https://github.com/ramchandragawade/scribble-nest/',
+        demoUrl: 'https://scribblenest.ramchandragawade.in/',
         techStack: []
     }
 ]
